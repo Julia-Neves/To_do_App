7b29c74aa6d4ef62d9c7ce15b8d4efa9b0692dbd
Julia Neves <juliadr3@hotmail.com>
2021-04-22 22:10:28 -0300

    Funcionalidades sendo executadas com sucesso

--- src/controllers/tarefa-controller.js
+++ src/controllers/tarefa-controller.js
@@ -4,39 +4,50 @@ const TarefaDAO = require("../DAO/tarefas-dao");
 function tarefaController(app, bd) {
     const tarefaDAO = new TarefaDAO(bd);
 
-    app.get('/tarefa', (req, res) => {
-        tarefaDAO.listaTarefas()
-            .then((tarefa) => { res.send(tarefa) })
-            .catch((err) => res.send({mensagem: "Falha ao listar tarefas"}))
+    app.get('/tarefa', async (req, res) => {
+        try {
+            const listarTarefas = await tarefaDAO.listaTarefas()
+            res.send(listarTarefas);
+          } catch (err) {
+              res.status(404).send(err);
+          }
     });
 
     app.get('/tarefa/:id', (req, res) => {
         res.send({ mensagem: `você tentou buscar a Tarefa ${req.params.id}` })
     });
 
-    app.post('/tarefa', (req, res) => {
-        const body = req.body;
-        let assignment = new tarefaModel(0, body.titulo, body.descricao, body.status, body.datacriacao, body.id_usuario);
-        tarefaDAO.insereTarefa(assignment)
-            .then((mensagemSucesso) => res.status(201).send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem: mensagemFalha}))
-    });
+    app.post('/tarefa', async (req, res) => {
+        try {
+            const body = req.body;
+            let assignment = new tarefaModel(0, body.titulo, body.descricao, body.status, body.datacriacao, body.id_usuario);
+            const inserirTarefas = await tarefaDAO.insereTarefa(assignment)
+            res.send(inserirTarefas);
+        } catch (err) {
+            res.status(404).send(err); 
+        }
+});
 
-    app.delete('/tarefa/:id', (req, res) => {
-        const id = req.params.id
-        tarefaDAO.deletaTarefa(id)
-            .then((mensagemSucesso) => res.send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem : mensagemFalha}))
-
+    app.delete('/tarefa/:id', async (req, res) => {
+        try {
+            const email = req.params.email
+            const deletarTarefas = await tarefaDAO.deletaTarefa(email)
+            res.send(deletarTarefas);
+        } catch (err) {
+            res.status(404).send(err);
+        }
     });
 
     //Mudar tudo junto ao alterar tarefa, para nao precisar disso seria necessario usar o patch//
-    app.put('/tarefa/:id', (req, res) =>{
-        const id = req.params.id;
-        const body = req.body;
-        tarefaDAO.alteraTarefa(id, body)
-            .then((mensagemSucesso) => res.send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem : mensagemFalha}))
+    app.put('/tarefa/:id', async (req, res) =>{
+        try {
+            const email = req.params.email;
+            const body = req.body;
+            const alterarTarefas = await tarefaDAO.alteraTarefa(email, body)
+            res.send(alterarTarefas);
+        } catch (err) {
+            res.status(404).send(err);
+        }
     });
 };
 
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -8,43 +8,51 @@ function usuarioController(app, bd) {
 // Rota: Caminho + Verbo
 // GET /usuarios
 
-    app.get('/usuario', (req, res) => {
-        usuarioDAO.listaUsuarios()
-            .then((usuario) => { res.send(usuario) })
-            .catch((err) => res.send({mensagem: "Falha ao listar os usuarios"}))
+    app.get('/usuario', async (req, res) => {
+        try {
+          const listarUsuarios = await usuarioDAO.listaUsuarios()
+          res.send(listarUsuarios);
+        } catch (err) {
+            res.status(404).send(err);
+        }
     });
 
     app.get('/usuario/:email', (req, res) => {
         res.send({ mensagem: `você tentou buscar o Usuário chamado ${req.params.email}` })
     });
 
-    app.post('/usuario', (req, res) => {
-        const body = req.body;
-        let usuario = new usuarioModel(0, body.nome, body.email, body.senha);
-        usuarioDAO.insereUsuario(usuario)
-            .then((mensagemSucesso) => res.status(201).send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem: mensagemFalha}))
+    app.post('/usuario', async (req, res) => {
+        try {
+            const body = req.body;
+            let usuario = new usuarioModel(0, body.nome, body.email, body.senha);
+            const inserirUsuarios = await usuarioDAO.insereUsuario(usuario)
+            res.send(inserirUsuarios);
+          } catch (err) {
+              res.status(404).send(err); 
+          }
     });
 
-    app.delete('/usuario/:email', (req, res) => {
-        const email = req.params.email
-        usuarioDAO.deletaUsuario(email)
-            .then((mensagemSucesso) => res.send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem : mensagemFalha}))
-
+    app.delete('/usuario/:email', async (req, res) => {
+        try {
+            const email = req.params.email
+            const deletarUsuarios = await usuarioDAO.deletaUsuario(email)
+            res.send(deletarUsuarios);
+        } catch (err) {
+            res.status(404).send(err);
+        }
     });
  
     //Mudar a senha junto ao alterar nome// Teria que usar patch para nao ter que fazer isso//
-    app.put('/usuario/:email', (req, res) =>{
-        const email = req.params.email;
-        const body = req.body;
-        usuarioDAO.alteraUsuario(email, body)
-            .then((mensagemSucesso) => res.send({ mensagem : mensagemSucesso}))
-            .catch((mensagemFalha) => res.send({ mensagem : mensagemFalha}))
+    app.put('/usuario/:email', async (req, res) =>{
+        try {
+            const email = req.params.email;
+            const body = req.body;
+            const alterarUsuarios = await usuarioDAO.alteraUsuario(email, body)
+            res.send(alterarUsuarios);
+        } catch (err) {
+            res.status(404).send(err);
+        }
     });
-            
-
-        
 };
 
 module.exports = usuarioController;
